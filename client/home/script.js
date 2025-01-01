0d5d2de1c6ced2ad93399b774b97cfc664b839dc
Omada <137461829+omada15@users.noreply.github.com>
2025-01-01 19:09:35 +0000

    work on home screen, kinda on signup page

--- client/home/script.js
+++ client/home/script.js
@@ -2,39 +2,48 @@ let d = document;
 window.reqType = "Read"
 var pathd, datad;
 async function request(url, way, body) {
-    try {
-        const response = await fetch(url, {
-            method: way,
-            body,
-            headers: {
-                "Content-type": "application/JSON; charset=UTF-8",
-            },
-        });
-        const parsedResponse = await response.json();
-        return parsedResponse;
-    } catch (e) {
-        console.error(e);
-    }
+   try {
+      const response = await fetch(url, {
+         method: way,
+         body,
+         headers: {
+            "Content-type": "application/JSON; charset=UTF-8",
+         },
+      });
+      const parsedResponse = await response.json();
+      return parsedResponse;
+   } catch (e) {
+      console.error(e);
+   }
 }
 function g(id) {
-    return d.getElementById(id).value;
+   return d.getElementById(id).value;
 }
 function createEvent() {
-    var eventInfo = {
-        name: g("name"),
-        desc: g("desc"),
-        info: {
-            date: g("date"),
-            time: g("time"),
-            location: g(""),
-        }
-    }
+   var eventInfo = {
+      name: g("name"),
+      desc: g("desc"),
+      info: {
+         date: g("date"),
+         time: g("time"),
+         location: g(""),
+      }
+   }
 }
 function t() {
-    if (window.reqType === "Read") {
-        window.reqType = "Write";
-    } else {
-        window.reqType = "Read";
-    }
-    d.getElementById("td").innerHTML = window.reqType;
+   if (window.reqType === "Read") {
+      window.reqType = "Write";
+   } else {
+      window.reqType = "Read";
+   }
+   d.getElementById("td").innerHTML = window.reqType;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+   const toggleButton = document.getElementById("burger");
+   const sidebar = document.querySelector('.sidebar');
+
+   toggleButton.addEventListener('click', () => {
+      sidebar.classList.toggle('active');
+   });
+});
