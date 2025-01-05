ad817ccf31ea862469224ea8ac37d0d3c2624f21
Omada <137461829+omada15@users.noreply.github.com>
2025-01-05 04:40:23 +0000

    finish dropdown

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 const Navbar = ({ setCurrentPage }) => {
+   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+   const toggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+   };
    return (
       <nav className="bg-blue-600 p-4 text-white fixed w-full top-0 z-50">
          <div className="container mx-auto flex justify-between items-center">
-            <span className="text-xl font-bold">ventify</span>
-            <div className="space-x-4">
+            <span className="text-xl font-bold">eventify</span>
+            <div className="space-x-4 w-128">
                <button
                   onClick={() => setCurrentPage("home")}
                   className="hover:text-blue-200"
@@ -28,12 +35,25 @@ const Navbar = ({ setCurrentPage }) => {
                >
                   Dashboard
                </button>
-               <div>
-                  <button>
-                     Dropdown
-                     <i></i>
-                  </button>
-               </div>
+               <button onClick={toggleDropdown} className="hover:text-blue-200">
+                  Account
+               </button>
+               {isDropdownOpen && (
+                  <div className="absolute right-0 w-59 h-200 bg-blue-600 rounded-md shadow-lg z-10">
+                     <div
+                        onClick={() => setCurrentPage("login")}
+                        className="m-5 hover:text-blue-200"
+                     >
+                        Log In
+                     </div>
+                     <div
+                        onClick={() => setCurrentPage("register")}
+                        className="m-5 hover:text-blue-200"
+                     >
+                        Register
+                     </div>
+                  </div>
+               )}
             </div>
          </div>
       </nav>
